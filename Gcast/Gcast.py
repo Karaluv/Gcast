e221a56beade19237ccdddacb196ec597a95c49e
karaluv <egr.vrn@gmail.com>
2021-10-20 00:56:08 +0300

    ready texture

--- Gcast/Gcast.py
+++ Gcast/Gcast.py
@@ -5,141 +5,12 @@ import os
 import sys
 from pygame.locals import Color
 from pygame import display
-
+from render import rendering
 
 FPS = 60
 u = 0.5
 
-
-class wall:
-
-    def __init__(self,color,texture,p1,p2):
-        self.color = color
-        self.texture = texture
-        self.p1 = p1
-        self.p2 = p2
-
-class map:
-
-    def __init__(self, walls):
-        self.walls = walls
-
-
-        self.map_claster = [[1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,1],[1,0,0,0,1,0,0,0,1],[1,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1]]
-        #self.map_claster = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-        self.hm,self.wm = 5,9
-
-        self.freemap = [[0]*self.wm]*self.hm
-
-        
-
-    def ray_cast(self,density,dl,a0,a1,minR,maxR,x0,y0):
-        
-        global pe
-        wm,hm = self.wm,self.hm
-        mapC =[[0]*wm]*hm
-        mapP = [[0]*wm]*hm
-        render_data = []
-        a=a0
-        render_pol = []
-        while a < a1:
-            
-            mapP = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
-            
-            x , y = 0,0
-            render_data.append((1000,-1))
-            l = minR
-            
-            sin =math.sin(a)*dl
-            cos=math.cos(a)*dl
-
-            render_pol_buffer = []
-            
-            while l <maxR:
-
-
-                renderZ = self.map_claster[int(y+y0/stepy)][int(x+x0/stepx)]
-
-                if mapP[int(y+y0/stepy)][int(x+x0/stepx)] == 0:
-                    render_pol_buffer.append(l)
-                    mapP[int(y+y0/stepy)][int(x+x0/stepx)] += 1
-
-                if renderZ>0:
-                    
-
-
-                    mapC[int(y+y0/stepy)][int(x+x0/stepx)]=mapC[int(y+y0/stepy)][int(x+x0/stepx)]+1
-                    
-                    render_data[-1]=(l,mapC[int(y+y0/stepy)][int(x+x0/stepx)])
-
-                    #if mapC[int(y+y0/stepy)][int(x+x0/stepx)]==1:
-                    k = (  (round(y+y0/stepy)-y0/stepy-y)**2 +(round(x+x0/stepx)-x0/stepx-x)**2 )**0.5
-                    
-                    render_data[-1]=(l,mapC[int(y+y0/stepy)][int(x+x0/stepx)],k)
-
-
-                    render_pol.append(render_pol_buffer)
-
-
-
-                    break
-                y = y + sin
-                x = x + cos
-                l = l+dl
-            a = a + density
-        #print(len(render_data),len(render_pol))
-        return render_data,render_pol
-
-
-def texturize(render_data):
-    n = []
-    stone = []
-    n_=[]
-
-
-    for i in range(len(render_data)):
-
-        if render_data[i][1] == 1:
-            n.append(i)
-
-    n.append(len(render_data))
-    
-    for j in range(len(n)-1):
-        n_.append(n[j+1]-n[j])
-    x = 0
-    for j in range(len(n_)):
-
-        k1 = render_data[x][2]
-        k2 = abs(1-render_data[x+n_[j]-1][2])
-
-        #k2 = 1
-        #k1 = 0
-
-        if k2<k1:
-            k2,k1 = k1,k2
-
-        width,height = ston.get_width(), ston.get_height()
-        ston_ = pygame.Surface((width*(k2-k1) , height ))
-        ston_.blit(ston,(0,0),(k1*width,0,width,height))
-
-        width,height = ston_.get_width(), ston_.get_height()
-        #ston_ = ston
-        for i in range(n_[j]):
-
-            stone.append(pygame.Surface((dw, int(2*pe/render_data[x][0])) ))
-            
-            stone[-1].blit(pygame.transform.scale(ston_,(round(dw*n_[j]),int(2*pe/render_data[x][0]))),(0,0),(round(dw*i),0,dw,int(2*pe/render_data[x][0])))
-            dark = pygame.Surface(stone[-1].get_size()).convert_alpha()
-            dark.fill((0, 0, 0, render_data[x][0]*255/7))
-            stone[-1].blit(dark,(0,0))
-            x = x+1
-    return stone
-
- 
-
-class slave:
-    x = 0
-
+map = [[1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,1],[1,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1]]
 
 class billy:
     
@@ -159,125 +30,28 @@ class billy:
         x_ = self.x+l*math.sin(a)+n*math.cos(a)
         y_ = self.y+l*math.cos(a)+n*math.sin(a)
         global map1
-        if map1.map_claster[int(y_/100+0.25)][int(x_/100+0.25)] == 0:
-            if map1.map_claster[int(y_/100-0.25)][int(x_/100-0.25)] == 0:
-                if map1.map_claster[int(y_/100-0.25)][int(x_/100+0.25)] == 0:
-                    if map1.map_claster[int(y_/100+0.25)][int(x_/100-0.25)] == 0:
+        if map[int(y_/100+0.25)][int(x_/100+0.25)] == 0:
+            if map[int(y_/100-0.25)][int(x_/100-0.25)] == 0:
+                if map[int(y_/100-0.25)][int(x_/100+0.25)] == 0:
+                    if map[int(y_/100+0.25)][int(x_/100-0.25)] == 0:
                         self.y = y_
                         self.x = x_
 
 
-def rotate_point(angle,p,po):
-    x_ = po[0] + math.cos(angle) * (p[0] - po[0]) - math.sin(angle) * (p[1] - po[1])
-    y_ = po[1] + math.sin(angle) * (p[0] - po[0]) + math.cos(angle) * (p[1] - po[1])
-    return (x_,y_)
-
-def draw_2d():
-    claster= map1.map_claster
-    x0 = pe
-    y0 = pe
-
-    for y in range(len(claster)):
-        for x in range(len(claster[y])):
-            if claster[y][x]!= 0:
-                
-                pygame.draw.rect(screen,(128,128,128),(x*x0,y*y0,x0,y0),2)
-
-def draw_pol(a0,a1,density,render_pol):
-    x,y = bill.x,bill.y
-    i = 0
-    
- 
-    w = 0
-    dw = W*density / (2*u) 
-
-    cl1 = (255,188,255)
-    cl2 =(100,100,255)
-
-    while w<W-1:
-
-        for j in range(len(render_pol[i])):
-
-            cl1_ = render_pol[i][j]/7*cl1[0],render_pol[i][j]/7*cl1[1],render_pol[i][j]/7*cl1[2]
-            cl2_ = render_pol[i][j]/7*cl2[0],render_pol[i][j]/7*cl2[1],render_pol[i][j]/7*cl2[2]
-
-            cl1__ = cl1[0] - cl1_[0],cl1[1] - cl1_[1],cl1[2] - cl1_[2]
-            cl2__ = cl2[0] - cl2_[0],cl2[1] - cl2_[1],cl2[2] - cl2_[2]
-
-            pygame.draw.line(screen,cl1__,(w-i,H//2+ pe/render_pol[i][j]),(w+dw-i,H//2+ pe/render_pol[i][j]),2)
-            pygame.draw.line(screen,cl2__,(w-i,H//2- pe/render_pol[i][j]),(w+dw-i,H//2 -  pe/render_pol[i][j]),2)
-        w = w+dw
-        i=i+1
-
-def draw_wall(a0,a1,density,render_data):
-
-    x,y = bill.x,bill.y
-    i = 0
-    
- 
-    w = 0
-    dw = W*density / (2*u) 
-
-    stone = texturize(render_data)
-
-
-    while w < W-1:
-        screen.blit(stone[i],(w-i,int(H//2-pe/render_data[i][0])))
-        #pygame.draw.rect(screen,(128,255,128),(w,H//2-pe/render_data[i][0],dw,2*pe/render_data[i][0]),0)
-        w = w+dw
-        
-        i = i+1
-
-        
-def draw_ray2d(a0,a1,density,render_data):
-    x,y = bill.x,bill.y
-    i = 0
-
-    cl = (255,128,128)
-
-    a = a0
-
-    while a < a1:
-
-        
-        
-        pygame.draw.line(screen,cl_,(x,y),(x+math.cos(a)*render_data[i][0]*pe,y+math.sin(a)*render_data[i][0]*pe),2)
-        i = i+1
-        a = a+ density
-
-
-def render():
-    
-    for i in map1.index:
-        draw_wall(map1.walls[i],map1.l1[i],map1.l2[i])
 
 def start():
     global pe
+
+    rend = rendering(0.5,0.01,screen)
+
     print("start")
     bill = billy(150,150,-math.pi/2,"VAn")
-    wall1 = wall((128,128,128),False,(-100,200),(100,200))
-    wall2 = wall((128,128,0),False,(-100,100),(100,100)) 
-    walls = [wall1,wall2]
-    return  map(walls), bill
+    return  rend,bill 
 
 def input(user_input):
 
 
     keys=pygame.key.get_pressed()
-
-    #if keys[K_s]:
-    #   map1.Move(10,0)
-    #if keys[K_w]:
-    #   map1.Move(-10,0)
-    #if keys[K_d]:
-    #   map1.Move(0,10)
-    #if keys[K_a]:
-    #   map1.Move(0,-10)
-
-    #if keys[K_q]:
-    #   map1.Rotation_map(0.01,(0,0))
-    #if keys[K_e]:
-    #   map1.Rotation_map(-0.01,(0,0))
 
 
     for event in user_input:
@@ -327,25 +101,13 @@ def input(user_input):
 
 
 def update():
-
-    #screen.blit(nacl(pygame.transform.rotate(floor,bill.a),0.8),(0,H//2))
-    
-
-    gradientRect(screen,(0,0,0),(128,50,128),pygame.Rect(0,H//2,W,H//2))
-    gradientRect(screen,(128,50,50),(0,0,0),pygame.Rect(0,0,W,H//2))
     
     a = bill.a
 
-    
-    
-    render_data,render_pol = map1.ray_cast(0.01,0.01,a-u,a+u,0.01,600,bill.x,bill.y)
+    rend.render(map,0.01,0.01,a-u,a+u,0.01,7,bill.x,bill.y)
 
-    
-    
-    draw_wall(a-u,a+u,0.01,render_data)
-    draw_pol(a-u,a+u,0.01,render_pol)
-    #draw_ray2d(a - u,a + u,0.01,render_data)
-    #draw_ray2d(a-u,a+u,0.01,render_data)
+
+
 
 pygame.init()
 
@@ -373,6 +135,8 @@ i =0
 move = False
 
 
+
+
 #screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h),pygame.FULLSCREEN)
 
 screen = pygame.display.set_mode((infoObject.current_w // 2, infoObject.current_h // 2))
@@ -381,7 +145,7 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 bl = (0,0,0)
-map1,bill = start()
+rend,bill = start()
 L0 = 25
 pe =200
 stepx =100
--- Gcast/render.py
+++ Gcast/render.py
@@ -1,4 +1,294 @@
 import pygame
 import math
+from random import randint
+import os
+import sys
+from pygame.locals import Color
+from pygame import display
 
+
+FPS = 60
+u = 0.5
+
+
+
+class rendering:
+
+    def __init__(self,u,density,render_surface):
+
+        self.render_surface = render_surface
+
+        self.u = u
+        self.density = density
+        W,H = render_surface.get_size()
+        self.W , self.H = W ,H
+        self.dw = W*density / (2*u) 
+
+        self.ston = pygame.image.load(os.path.join(sys.path[0],"stone.png"))
+        floor =  pygame.image.load(os.path.join(sys.path[0],"floor.jpg"))
+        self.floor = pygame.transform.scale(floor,(int(W*8.76/10.1),H//2))
+
+        self.pe = 200
+        self.stepx =100
+        self.stepy = 100
+
+
+        
+    def ray_cast(self,map,density,dl,a0,a1,minR,maxR,x0,y0):
+        
+        pe = self.pe
+
+        blockpos = (0,0)
+
+        stepx = self.stepx
+        stepy= self.stepy
+    
+        mapC =[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+        mapP = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+
+        render_data = []
+        render_pol = []
+
+        a = a0
+
+        while a < a1:
+            
+            mapP = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+            
+            x , y = 0,0
+            render_data.append([8,1,1])
+            l = minR
+            
+            sin =math.sin(a)*dl
+            cos=math.cos(a)*dl
+
+            up = True
+            left =True
+            right = True
+            down = True
+
+            render_pol_buffer = []
+            previous = (0,0)
+
+            yp,xp = int(y+y0/stepy),int(x+x0/stepx)
+            
+
+            #if abs((int(y0/stepy)-y0/stepy) / sin)<abs((int(x0/stepx)-x0/stepx) / cos):
+             #   k = 1
+            #else:
+             #   k =-1
+            
+
+            
+
+            while l <maxR:
+
+                
+
+                renderZ = map[int(y+y0/stepy)][int(x+x0/stepx)]
+
+                if mapP[int(y+y0/stepy)][int(x+x0/stepx)] == 0:
+                    mapP[int(y+y0/stepy)][int(x+x0/stepx)] += 1
+                    
+                    render_pol_buffer.append(l)
+
+                #if yp !=int(y+y0/stepy) or xp != int(x+x0/stepx):
+                    #k = -k
+
+                
+
+                if renderZ>0:
+                    #k = -k
+
+
+                    dx = int(x+x0/stepx) - x - x0/stepx
+                    dy = int(y+y0/stepy) - y - y0/stepy
+
+                    k = 0
+
+                    if yp >int(y+y0/stepy):
+                        k = abs(dx)
+                    if yp <int(y+y0/stepy):
+                        k = 1-abs(dx)
+
+                    if xp <int(x+x0/stepx):
+                        k = abs(dy)
+                    if xp >int(x+x0/stepx):
+                        k = 1-abs(dy)
+
+
+
+                    #if a<=math.pi/2:
+                    #    dy = int(y+y0/stepy) - y - y0/stepy
+
+                    #if a>math.pi/2:
+
+                   
+
+
+                    
+
+                    mapC[int(y+y0/stepy)][int(x+x0/stepx)] = mapC[int(y+y0/stepy)][int(x+x0/stepx)] + 1
+                    render_pol.append(render_pol_buffer)
+                    render_data[-1]=[l, mapC[int(y+y0/stepy)][int(x+x0/stepx)],k]
+                    
+
+                    break
+
+                yp,xp = int(y+y0/stepy),int(x+x0/stepx)
+
+                y = y + sin
+                x = x + cos
+                l = l+dl
+
+                
+
+            a = a + density
+            #print(len(render_data),len(render_pol))
+        return render_data,render_pol
+
+
+    def texturize(self,render_data):
+
+        n = []
+        stone = []
+        n_=[]
+
+        ston = self.ston
+        dw = self.dw
+        pe = self.pe
+
+        for i in range(len(render_data)):
+
+            if render_data[i][1] == 1:
+                n.append(i)
+
+        n.append(len(render_data))
+    
+        for j in range(len(n)-1):
+            n_.append(n[j+1]-n[j])
+        x = 0
+        for j in range(len(n_)):
+
+            k1 = render_data[x][2]
+            k2 = render_data[x+n_[j]-1][2]
+
+
+            
+
+            if k2<k1:
+              k2,k1 = k1, k2
+
+            #k1 = 0
+            #k2 = 1
+
+
+
+
+
+
+            width,height = ston.get_width(), ston.get_height()
+            ston_ = pygame.Surface((width*(k2-k1) , height ))
+            ston_.blit(ston,(0,0),(k1*width,0,width,height))
+
+            width,height = ston_.get_width(), ston_.get_height()
+            #ston_ = ston
+            for i in range(n_[j]):
+
+                stone.append(pygame.Surface((dw, int(2*pe/render_data[x][0])) ))
+            
+                stone[-1].blit(pygame.transform.scale(ston_,(round(dw*n_[j]),int(2*pe/render_data[x][0]))),(0,0),(round(dw*i),0,dw,int(2*pe/render_data[x][0])))
+                dark = pygame.Surface(stone[-1].get_size()).convert_alpha()
+                dark.fill((0, 0, 0, render_data[x][0]*255/7))
+                stone[-1].blit(dark,(0,0))
+                x = x+1
+        return stone
+
+ 
+
+    def draw_poll(self,a0,a1,density,render_pol):
+        i = 0
+    
+ 
+        w = 0
+        
+        W = self.W
+        H = self.H
+        pe = self.pe
+        dw = self.dw
+
+        cl1 = (255,188,255)
+        cl2 =(100,100,255)
+
+        while w<W-1:
+
+            for j in range(len(render_pol[i])):
+
+                cl1_ = render_pol[i][j]/7*cl1[0],render_pol[i][j]/7*cl1[1],render_pol[i][j]/7*cl1[2]
+                cl2_ = render_pol[i][j]/7*cl2[0],render_pol[i][j]/7*cl2[1],render_pol[i][j]/7*cl2[2]
+
+                cl1__ = cl1[0] - cl1_[0],cl1[1] - cl1_[1],cl1[2] - cl1_[2]
+                cl2__ = cl2[0] - cl2_[0],cl2[1] - cl2_[1],cl2[2] - cl2_[2]
+
+                pygame.draw.line(self.render_surface,cl1__,(w-i,H//2+ pe/render_pol[i][j]),(w+dw-i,H//2+ pe/render_pol[i][j]),2)
+                pygame.draw.line(self.render_surface,cl2__,(w-i,H//2- pe/render_pol[i][j]),(w+dw-i,H//2 -  pe/render_pol[i][j]),2)
+            w = w+dw
+            i=i+1
+
+
+    def draw_wall(self,a0,a1,density,render_data):
+
+        W= self.W
+        H = self.H
+        pe = self.pe
+        dw = self.dw
+        i = 0
+    
+ 
+        w = 0
+        
+
+        stone = self.texturize(render_data)
+
+
+        while w < W-1:
+            self.render_surface.blit(stone[i],(w-i,int(H//2-pe/render_data[i][0])))
+            #pygame.draw.rect(self.render_surface,(128,255,128),(w,H//2-pe/render_data[i][0],dw,2*pe/render_data[i][0]),0)
+            w = w+dw
+        
+            i = i+1
+
+    def draw_background(self):
+        W,H = self.W,self.H
+        self.gradientRect(self.render_surface,(0,0,0),(128,50,128),pygame.Rect(0,H//2,W,H//2))
+        self.gradientRect(self.render_surface,(128,50,50),(0,0,0),pygame.Rect(0,0,W,H//2))
+
+
+    def render(self,map,density,dl,a0,a1,minR,maxR,x0,y0):
+
+        render_wall_data , render_poll_data = self.ray_cast(map,density,dl,a0,a1,minR,maxR,x0,y0)
+
+        self.draw_background()
+        self.draw_wall(a0,a1,density,render_wall_data)
+        self.draw_poll(a0,a1,density,render_poll_data)
+
+
+
+
+
+
+
+
+    def gradientRect( self,window, left_colour, right_colour, target_rect ):
+        """ Draw a horizontal-gradient filled rectangle covering <target_rect> """
+        colour_rect = pygame.Surface( ( 2, 2 ) )                                   # tiny! 2x2 bitmap
+        pygame.draw.line( colour_rect, left_colour,  ( 0,0 ), ( 1,0 ) )            # left colour line
+        pygame.draw.line( colour_rect, right_colour, ( 0,1 ), ( 1,1 ) )            # right colour line
+        colour_rect = pygame.transform.smoothscale( colour_rect, ( target_rect.width, target_rect.height ) )  # stretch!
+        window.blit( colour_rect, target_rect )                                    # paint it
+
+
+
+
+    
 
